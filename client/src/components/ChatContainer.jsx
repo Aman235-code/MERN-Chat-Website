fc5aca19f98a125dbe8f8f74f11625baf6ebb3f2
Aman Ahamed <amanahamed04@gmail.com>
2026-01-10 19:24:18 +0530

    chat container

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import assets, { messagesDummyData } from "../assets/assets";
+import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = ({ selectedUser, setSelectedUser }) => {
+
+  const scrollEnd = useRef()
+
+  useEffect(()=> {
+      if(scrollEnd.current){
+        scrollEnd.current.scrollIntoView({behavior: "smooth"})
+      }
+  },[])
+
+
   return selectedUser ? (
     <div className="h-full overflow-scroll relative backdrop-blur-lg">
       <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
@@ -25,8 +36,25 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
             className={`flex items-end gap-2 justify-end ${
               msg.senderId !== "680f50e4f10f3cd28382ecf9" && "flex-row-reverse"
             }`}
-          ></div>
+          >
+            {msg.image?(
+              <img className="max-w-57.5 border border-gray-700 rounded-lg overflow-hidden mb-8" src={msg.image} alt="image"/>
+            ):(
+              <p className={`p-2 max-w-50 md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30
+                text-white ${msg.senderId === '680f50e4f10f3cd28382ecf9' ? 'rounded-br-none': 'rounded-bl-none'}`}>{msg.text}</p>
+            )}
+
+            <div className="text-center text-xs">
+              <img src={msg.senderId ==='680f50e4f10f3cd28382ecf9'? assets.avatar_icon: assets.profile_martin } alt="" className="w-7 rounded-full" />
+              <p className="text-gray-500">{ formatMessageTime(msg.createdAt)}</p>
+            </div>
+
+
+          </div>
         ))}
+        <div ref={scrollEnd}>
+
+        </div>
       </div>
     </div>
   ) : (
